a529e41c4e1592b9af079f494587aa2cd46cee01
Wang-Jingyi09 <20104728@mail.wit.ie>
2024-01-02 16:17:17 +0800

    fixed: use global path for static of express

--- movies-api/app.js
+++ movies-api/app.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('build'));
+app.use(express.static(path.resolve(__dirname, './build')));
 //app.use(authenticate);
 
 
